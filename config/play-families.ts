87f871901789a0d1b0df99114bd2592f000d47db
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2024-05-04 10:07:34 -0500

    play families array

---
+++ config/play-families.ts
@@ -0,0 +1,12 @@
+const playFamilies = [
+  "Outside Zone",
+  "Inside Zone",
+  "Pin and Pull",
+  "Power",
+  "Quick Game",
+  "Dropback",
+  "Play Action Dropback",
+  "Play Action Deep",
+  "Screen",
+  "Naked"
+];
