5922f4a406dc9aedb46e9afd1104df62a28413fe
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2024-05-12 12:31:28 -0500

    cleaning up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,26 @@
 "use client"
 
+import Image from "next/image"
 import { useFormStatus } from "react-dom"
 import { Login } from "./action"
 
 export default function Page() {
   return (
-    <form action={Login}>
-      <input type="email" name="email" placeholder="Email" />
-      <input type="password" name="password" placeholder="Password" />
-      <SubmitButton />
-    </form>
+    <>
+      <div className="flex flex-col items-center gap-4">
+        <div className="flex gap-2 items-center select-none cursor-default">
+          <Image src="/qblogs_logo_lightmode.svg" alt="logo" width={44} height={44} />
+          <span className="font-bold text-lg">QB Logs</span>
+        </div>
+        <div>
+          <form action={Login}>
+            <input type="email" name="email" placeholder="Email" />
+            <input type="password" name="password" placeholder="Password" />
+            <SubmitButton />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
