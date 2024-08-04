5638c77b135fb099320d5b72774e065bdf450d02
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2024-08-04 11:17:39 -0500

    types in data folder

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,9 +1,7 @@
-import { Database } from "@/types/supabase";
 import { createBrowserClient } from "@supabase/ssr";
-import process from "process";
 
 export const createClient = () =>
-  createBrowserClient<Database>(
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,4 +1,3 @@
-import { Database } from '@/types/supabase'
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import process from 'process'
@@ -6,7 +5,7 @@ import process from 'process'
 export function createClient() {
   const cookieStore = cookies()
 
-  return createServerClient<Database>(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
